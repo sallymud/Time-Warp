695181b7af16cc1241452aadb6875d350742302d
sallymud <Nepogodina04@mail.ru>
2023-04-15 00:31:12 +0500

    add animation and new scene without elements

---
+++ Assets/Game-Scripts/BottomBarController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class BottomBarController : MonoBehaviour
+{
+    public TextMeshProUGUI barText;
+    public TextMeshProUGUI personNameText;
+
+    private int sentenceIndex = -1;
+    public StoryScene currentScene;
+    private State state = State.COMPLETED;
+
+    private enum State
+    {
+        PLAYING, COMPLETED
+    }
+
+    void Start()
+    {
+        StartCoroutine(TypeText(currentScene.sentences[++sentenceIndex].text));
+    }
+
+    private IEnumerator TypeText(string text)
+    {
+        barText.text = "";
+        state = State.PLAYING;
+        int wordIndex = 0;
+
+        while (state != State.COMPLETED)
+        {
+            barText.text += text[wordIndex];
+            yield return new WaitForSeconds(0.05f);
+            if (++wordIndex == text.Length)
+            {
+                state = State.COMPLETED;
+                break;
+            }
+        }
+    }
+}
